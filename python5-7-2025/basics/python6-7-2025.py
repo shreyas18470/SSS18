44e7992f06d78b50e012c9d251be4ec07f463503
Admin <Admin@DESKTOP-0R5LQDM>
2025-07-06 20:16:55 +0530

    class 6-7-20225

---
+++ python5-7-2025/basics/python6-7-2025.py
@@ -0,0 +1,14 @@
+'''
+Created on 06-Jul-2025
+'''
+num1 = 60
+num2 = 100
+print(f"sum of {num1} and {num2} =",num1+num2)
+person = "shreyas"
+print("name-",person)
+location = "mysuru"
+print("place-",location)
+
+
+
+
