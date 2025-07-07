bdee1309217f90fe88d9c34ec6d6141d5ba1030c
Admin <Admin@DESKTOP-0R5LQDM>
2025-07-07 12:41:35 +0530

    types of data

---
+++ python5-7-2025/basics/python7-7-2025.py
@@ -0,0 +1,25 @@
+'''
+'''
+student = "shreyas" 
+roll_no = 45 # integer
+height = 5.10 # float
+it_raining = True # boolean
+its_cold = False # boolean
+i_like_movie = None # nonetype
+a = 10+10j 
+
+print("type of student is ",type(student))
+print("type of roll no",type(roll_no))
+print("type of height is",type(height))
+print("type of it_raining",type(it_raining))
+print("type of its_cold",type(its_cold))
+print("type of i_like_movie",type(i_like_movie))
+print('type of a',type(a))
+
+print('=====================')
+#print("roll_no after converting to string",string(roll_no))
+
+print("=============----------")
+print("roll_no after converting to ",float(roll_no))
+
+print('//////////////////////////////')
