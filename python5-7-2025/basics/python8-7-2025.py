7a0ae9518bb5963993afc58cefc4551688c9cf7b
Admin <Admin@DESKTOP-0R5LQDM>
2025-07-08 10:53:01 +0530

    data input 8-7-2025

---
+++ python5-7-2025/basics/python8-7-2025.py
@@ -0,0 +1,18 @@
+'''
+Created on 08-Jul-2025
+
+@author: Admin
+'''
+name=(input('enter your name:'))
+print("hello,",(name))
+a=(input("how was your day today"))
+print("",(a))
+b=int(input("please enter your id"))
+print("",(b))
+c=(input("enter your passward"))
+print("",(c))
+d=int(input('type a number'))
+e=float(input('retype an number'))
+#print("sum of ",(d))
+#print('sum of',(e))
+print('add {d} and {e}',(d+e))
